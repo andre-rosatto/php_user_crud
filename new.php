0251df59a34beb7424614a9c99716b43a69ed52b
André Rosatto <linus1178@gmail.com>
2024-07-24 18:06:02 -0300

    v1

--- new.php
+++ new.php
@@ -33,15 +33,15 @@
 	<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
 		<h3 class="center">New User</h3>
 		<label for="name">Name:</label>
-		<input required type="text" name="name" value="<?php echo $user['name']; ?>">
+		<input required type="text" name="name" id="name" value="<?php echo $user['name']; ?>">
 		<div class="red-text"><?php echo $errors['name'] ?? ''; ?></div>
 		
-		<label for="name">Email:</label>
-		<input required type="email" name="email" value="<?php echo $user['email']; ?>">
+		<label for="email">Email:</label>
+		<input required type="email" name="email" id="email" value="<?php echo $user['email']; ?>">
 		<div class="red-text"><?php echo $errors['email'] ?? ''; ?></div>
 		
-		<label for="name">Birth date:</label>
-		<input required type="date" name="birth_date" value="<?php echo $user['birth_date']; ?>">
+		<label for="birth_date">Birth date:</label>
+		<input required type="date" name="birth_date" id="birth_date" value="<?php echo $user['birth_date']; ?>">
 
 		<div class="center">
 			<input class="btn" type="submit" value="OK" name="submit">
